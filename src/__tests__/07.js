fef39a67eb66591ca85636db59ff2d66a3325bc3
Luca Panzavolta <lvolta@protonmail.com>
2020-09-27 12:27:10 +0200

    Exercise 7 completed. All tests passing.

--- src/__tests__/07.js
+++ src/__tests__/07.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {renderToggle, fireEvent} from '../../test/utils'
-import Usage, { Toggle } from '../exercises-final/07'
-// import Usage, { Toggle } from '../exercises/07'
+// import Usage, { Toggle } from '../exercises-final/07'
+import Usage, { Toggle } from '../exercises/07'
 
 test('renders a toggle component', () => {
   const handleToggle = jest.fn()
@@ -31,18 +31,3 @@ test('toggle uses default props', () => {
   expect(typeof Toggle.defaultProps.initialOn).toBe('boolean')
   expect(typeof Toggle.defaultProps.onReset).toBe('function')
 })
-
-//////// Elaboration & Feedback /////////
-// When you've finished with the exercises:
-// 1. Copy the URL below into your browser and fill out the form
-// 2. remove the `.skip` from the test below
-// 3. Change submitted from `false` to `true`
-// 4. And you're all done!
-/*
-http://ws.kcd.im/?ws=react%20patterns&e=07&em=
-*/
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(submitted).toBe(true)
-})
-////////////////////////////////
