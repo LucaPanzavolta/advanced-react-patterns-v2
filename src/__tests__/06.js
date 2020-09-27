dfccbccc3e1fe38f396cb93f404f16e509eb21f3
Luca Panzavolta <lvolta@protonmail.com>
2020-09-27 11:29:22 +0200

    Exercise 6 completed.

--- src/__tests__/06.js
+++ src/__tests__/06.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {renderToggle, fireEvent} from '../../test/utils'
-import Usage from '../exercises-final/06'
-// import Usage from '../exercises/06'
+// import Usage from '../exercises-final/06'
+import Usage from '../exercises/06'
 
 test('renders a toggle component', () => {
   const handleToggle = jest.fn()
@@ -46,18 +46,3 @@ test('passes custom props to the custom-button', () => {
   expect(handleToggle).toHaveBeenCalledTimes(1)
   expect(handleToggle).toHaveBeenCalledWith(true)
 })
-
-//////// Elaboration & Feedback /////////
-// When you've finished with the exercises:
-// 1. Copy the URL below into your browser and fill out the form
-// 2. remove the `.skip` from the test below
-// 3. Change submitted from `false` to `true`
-// 4. And you're all done!
-/*
-http://ws.kcd.im/?ws=react%20patterns&e=06&em=
-*/
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(submitted).toBe(true)
-})
-////////////////////////////////
--- src/exercises/06.js
+++ src/exercises/06.js
@@ -13,7 +13,7 @@ import {Switch} from '../switch'
 // with the ones we need to get our toggle functionality to work
 //
 // 💰 Here's a little utility that might come in handy
-// const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args))
+const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args))
 
 class Toggle extends React.Component {
   state = {on: false}
@@ -26,10 +26,16 @@ class Toggle extends React.Component {
     return {
       on: this.state.on,
       toggle: this.toggle,
-      togglerProps: {
-        'aria-pressed': this.state.on,
-        onClick: this.toggle,
-      },
+      getTogglerProps: (propsToMerge) => {
+        const mergedOnClickFn = typeof propsToMerge.onClick === "function" ? 
+          callAll(propsToMerge.onClick, this.toggle) : 
+          this.toggle;
+          
+        return {
+          ...propsToMerge, 
+          onClick: mergedOnClickFn,
+        }
+      }
     }
   }
   render() {
